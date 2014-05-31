11be6a9a59bf9b8dd7a03f3278a400a48740341a
Rcamach <rudycamacho@Rudys-MacBook-Pro.local>
2014-05-31 17:32:11 -0600

    added numbers.rb challenge

---
+++ numbers.rb
@@ -0,0 +1,26 @@
+puts "Lets do some math!"
+
+puts "25 + 30 / 6 is "
+puts  25 + 30 / 6
+
+puts "100 - 25 * 3 / 4 is "
+puts 100 - 25 * 3 / 4
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+puts 3 + 2 < 5 - 7
+
+puts "Is 5 greater than -2? "
+puts 5 > -2
+
+puts "Is 5 greater or equal to -2? "
+puts 5 >= -2
+
+puts "Is 5 less than or equal to -2? "
+puts 5 <= -2
+
+number1 = 10
+number2 = 5
+number3 = 2
+
+puts "Is 10 + 5 > 2?"
+puts 10 + 5 > 2
